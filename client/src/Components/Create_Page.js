17ecb0ae967c93a959ac287a0f54b70dcf8f66f7
Jegan.V.G <jegan7201_ai@mepcoeng.ac.in>
2022-10-08 22:13:22 +0530

    added pages

--- client/src/Components/Create_Page.js
+++ client/src/Components/Create_Page.js
@@ -20,6 +20,11 @@ function CreatePage () {
                 <br></br>
                 <br></br>
                 <br></br>
+                <form action="http://localhost:3000/api/movie" method="post">
+                    <input type="text" name="name">NAME</input><br></br>
+                    <input type="text" name="time">TIMING</input><br></br>
+                    <input type="number" name="rating">RATING</input><br></br>
+                </form>
                 <textarea className="textarea" placeholder="Type Your Create Query Here..." required></textarea><br></br><br></br>
                 <Button variant="contained" className="Create" color="error">Create</Button><br></br><br></br>
             </div>
